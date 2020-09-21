cb9c7456c6392151d1c60a45529876c2b567d7bd
Darko Draskovic <darko.draskovic@gmail.com>
2020-09-21 20:59:17 +0200

    Add 2600 macro files
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

---
+++ colorbg/colorbg.asm
@@ -0,0 +1,20 @@
+        processor 6502
+
+        include "../atari2600/vcs.asm"
+        include "../atari2600/macro.asm"
+
+        seg code
+        org $F000               ; defines the origin of the ROM
+
+        CLEAN_START             ; clears stack, all TIA registers and RAM to 0
+
+Start:          
+        lda #$A4                ; load col val in A ($1E is NTSC yellow)
+        sta COLUBK              ; store A to to BackgroundColor Address $09
+
+        jmp Start
+        
+        org $FFFC
+        .word Start             ; reset vector at $FFFC (where prog starts)
+        .word Start             ; interrupt vector at $FFFE (unused in VCS)
+        
