ce49ab6fe2fcff8fee07510ee8ff36baf5cc5220
Darko Draskovic <darko.draskovic@gmail.com>
2020-10-20 19:22:24 +0200

    Add .lst and .sym files to cmd line opts
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- colorbg/colorbg.asm
+++ colorbg/colorbg.asm
@@ -7,9 +7,9 @@
         org $F000               ; defines the origin of the ROM
 
         CLEAN_START             ; clears stack, all TIA registers and RAM to 0
-
+        
 Start:          
-        lda #$A4                ; load col val in A ($1E is NTSC yellow)
+        lda #$5A                ; load col val in A ($1E is NTSC yellow)
         sta COLUBK              ; store A to to BackgroundColor Address $09
 
         jmp Start
