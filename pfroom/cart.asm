709a3558441b6eb7a77029a7c7c9863180ea6da1
Darko Draskovic <darko.draskovic@gmail.com>
2020-12-10 20:43:40 +0100

    Add preprocessor REPEAT directive
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- pfroom/cart.asm
+++ pfroom/cart.asm
@@ -15,26 +15,22 @@ NextFrame:
 
         lda #2
         sta VSYNC               ; turn on VSYNC
-
+        sta VBLANK              ; turn on VBLANK
+        
         ;; generate 3 lines of VSYNC
-        sta WSYNC               ; first scanline
-        sta WSYNC               ; second scanline
-        sta WSYNC               ; third scanline
+        REPEAT 3
+                sta WSYNC               ; second scanline
+        REPEND
 
         lda #0
         sta VSYNC               ; turn off VSYNC
 
 ;;; VBLANK
 
-        lda #2
-        sta VBLANK              ; turn on VBLANK
-
         ;; let TIA output 37 scanlines of VBLANK
-        ldx #37                 ; X = 37 (to count 37 scanlines)
-LoopVBlank:
+        REPEAT 37
         sta WSYNC               ; hit WSYNC and wait for the next scanline
-        dex                     ; x--
-        bne LoopVBlank          ; loop while X != 0
+        REPEND
 
         lda #0
         sta VBLANK              ; turn off VBLANK
@@ -47,56 +43,47 @@ LoopVBlank:
         ldx #$84
         stx COLUPF              ; set the playfield color
         ldy 1
-        sty CTRLPF              ; reflect playfield left half
+        sty CTRLPF              ; reflect playfield left half (CTRLPF register > D0 means reflect)
 
 ;;; 7 scanlines of no playfield
-        ldx #7
-LoopPF1:
-        sta WSYNC               ; wait for next scanline
-        dex                     ; x--
-        bne LoopPF1             ; loop while X != 0
+        REPEAT 7
+                sta WSYNC       ; wait for next scanline
+        REPEND
 
 ;;; 7 scanlines of reflected horizontal line
         ldy #$e0                ; 0xe0 = 0b11100000 - first four bits are used and reversed to yield a pattern
         sty PF0
         ldy #$ff                ; 0xff = 0b11111111
         sty PF1
-        ldy #$ff                ; 0xff = 0b11111111
         sty PF2
 
-        ldx #7
-LoopPF2:
-        sta WSYNC               ; wait for next scanline
-        dex                     ; x--
-        bne LoopPF2             ; loop while X != 0
+        REPEAT 7
+                sta WSYNC               ; wait for next scanline
+        REPEND
 
 ;;; 164 scanlines of reflected vertical line
-        ldy #$20                ; 0x20 = 00100000 - first four bits are used and reversed to yield a pattern
+        ldy #%00100000          ; high nibble is reversed to yield a pattern; low nibble is ignored
         sty PF0
         ldy #0
         sty PF1
+        ldy #%10000000        
         sty PF2
 
-        ldx #164
-LoopPF3:
-        sta WSYNC               ; wait for next scanline
-        dex                     ; x--
-        bne LoopPF3             ; loop while X != 0
+        REPEAT 164
+                sta WSYNC               ; wait for next scanline
+        REPEND
 
 
 ;;; 7 scanlines of reflected horizontal line
         ldy #$e0
         sty PF0
-        ldy #$ff
+        ldy #%11111111
         sty PF1
-        ldy #$ff
         sty PF2
 
-        ldx #7
-LoopPF4:
-        sta WSYNC               ; wait for next scanline
-        dex                     ; x--
-        bne LoopPF4             ; loop while X != 0
+        REPEAT 7
+                sta WSYNC               ; wait for next scanline
+        REPEND
 
 ;;; 7 scanlines of no playfield
         ldy #0
@@ -104,22 +91,18 @@ LoopPF4:
         sty PF1
         sty PF2
 
-        ldx #7
-LoopPF5:
-        sta WSYNC
-        dex
-        bne LoopPF5
+        REPEAT 7
+                sta WSYNC
+        REPEND
 
 ;;; Output 30 VBLANK lines (overscan) to complete frame
 
         lda #2
         sta VBLANK              ; turn on VBLANK
 
-        ldx #30                 ; counter for 30 scanlines
-LoopOverscan:
-        sta WSYNC               ; wait for the next scanline
-        dex                     ; X--
-        bne LoopOverscan        ; loop while X != 0
+        REPEAT 30
+                sta WSYNC               ; wait for the next scanline
+        REPEND
 
         jmp NextFrame
 
